b913605a1d098a0fb0ff4652f39d268a9133796d
yehonatanhal <yehonatanhal@users.noreply.github.com>
2025-09-09 14:48:42 +0300

    bugfix + remove image api client (#8)
    * * enable configurtion
    * dynamic filter of essences
    
    * remove image api + fix little bug

--- apps/api/trpc/routers/tree-api-service.ts
+++ apps/api/trpc/routers/tree-api-service.ts
@@ -1,19 +1,14 @@
 import { publicProcedure, router } from '../init';
 import { TreeApiClient } from '../../services/tree-api-client';
-import { ImageApiClient } from '../../services/image-api-client';
 import type { TreeOfValuesResponse, TableEntity } from '../../types/tree-api-types';
-import type { ImageServiceResponse } from '../../types/image-api-types';
 import {
   getTreeOfValuesSchema,
   getAllTableEntitiesSchema,
   getTableEntitiesSchema,
-  getImageUrlSchema,
 } from './tree-api-validation-schemas';
 import { treeEntitiesConfig } from '../../config';
 
 const treeApiClient = TreeApiClient.getInstance();
-const imageApiClient = ImageApiClient.getInstance();
-
 
 export const treeEntitiesRouter = router({
   getTreeOfValues: publicProcedure
@@ -37,13 +32,6 @@ export const treeEntitiesRouter = router({
       const entities = await treeApiClient.getAllTableEntities(input);
       // Filter entities that have all required properties (only if requiredProperties is defined and not empty)
       return entities.filter(ent => 
-        treeEntitiesConfig.requiredProperties.every(propName => Boolean(ent.properties[propName])));      
-    }),
-
-  getImageUrl: publicProcedure
-    .input(getImageUrlSchema)
-    .query(async ({ input }): Promise<ImageServiceResponse> => {
-      console.log('tRPC getImageUrl called with exclusiveId:', input.exclusiveId);
-      return imageApiClient.getImageUrl(input.exclusiveId);
-    }),
+        treeEntitiesConfig.requiredProperties.every(propName => Boolean(ent.properties_list[propName])));      
+    })
 });
